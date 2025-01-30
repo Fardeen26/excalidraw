06ea728ed7bf90d0993642d56e5e909939e29ea0
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-30 22:34:51 +0530

    feat: added common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,18 +2,34 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from './config';
 import { middleware } from './middleware';
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from '@repo/common/types'
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/signup', async (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         userId: "123"
     })
 })
 
 app.post('/signin', async (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     const userId = 1;
 
     const token = jwt.sign({
@@ -26,6 +42,13 @@ app.post('/signin', async (req, res) => {
 })
 
 app.post('/room', middleware, async (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     res.json({
         roomId: 123
     })
